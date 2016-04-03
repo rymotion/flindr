9409c7478a62f66ff4d759a73fc407f52139b740
Jordan Ishii <jishii14@apu.edu>
2016-04-03 09:15:32 -0700

    added link to iTunes

--- flindr/flindr/APIConstantsUtil.swift
+++ flindr/flindr/APIConstantsUtil.swift
@@ -7,14 +7,21 @@
 //
 
 import Foundation
+import UIKit
 
 class APIConstantsUtil {
+    // Variables for MovieDb API calls
     static let URLBase = "https://api.themoviedb.org/3/"
     static let imageURLBase = "image.tmdb.org/t/p/w300/"
     static let movieGet = "movie/"
     static let apiKey = "api_key=954d7909c4c641cbf030293a45924bf9"
-    static let gbApiKey = "8EkTqrXQUiVwJsoyBnJHnQ2UzKrHUY"  // TEMPORARY (250 LIMIT)
     static var latestMovieID = 0 // Must be updated when opens
+    
+    // Variables for GuideBox API calls
+    static let gbURLBase = "https://api-public.guidebox.com/v1.43/US/"
+    static let gbApiKey = "8EkTqrXQUiVwJsoyBnJHnQ2UzKrHUY"  // TEMPORARY (250 LIMIT)
+    static let searchByGBKey = "/movie/"    // URL piece for finding based on GuideBox info
+    static let searchByMDBKey = "/search/movie/id/imdb/"    // URL piece for finding based on MovieDb
     
     // Data from latest random movie
     static var imgURL = NSURL()
@@ -105,6 +112,12 @@ class APIConstantsUtil {
                     // UPDATE UI HERE
                 }
             }
-            }.resume()
+        }.resume()
+    }
+    
+    // Links to iTunes
+    func linkToITunes() {
+        let url = NSURL(string: "itms://itunes.apple.com/")
+        UIApplication.sharedApplication().openURL(url!)
     }
 }
