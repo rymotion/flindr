7b8fce9908a419ffbab6b91eafc52f92b67ee09d
rymotion1@me.com <rymotion1@me.com>
2016-04-03 02:47:56 -0700

    Databaseupdate
    database update

---
+++ flindr/database.h
@@ -0,0 +1,13 @@
+//
+//  database.h
+//  flindr
+//
+//  Created by Ryan Paglinawan on 4/3/16.
+//  Copyright © 2016 RyanPaglinawan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "sqlite3.h"
+@interface database : NSObject
+
+@end
